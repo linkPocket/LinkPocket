8a057130c9ef173058753787a38134394a5a67e9
내 맥북에어 <>
2018-07-25 20:37:38 +0900

    URL 인지 아닌지 확인하는거 수정 :)

--- LinkPocket/LinkPocket/ViewController.swift
+++ LinkPocket/LinkPocket/ViewController.swift
@@ -12,10 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let pasteboard = UIPasteboard.general
         if let string = pasteboard.string {
-            if let _ = URL(string: string) {
+            if string.isValidURL {
                 print("\(string) is url")
             } else {
                 print("\(string) is not url")
@@ -29,6 +28,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+
+//MARK:- To check validity of URL
+extension String {
+    var isValidURL: Bool {
+        let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+        if let match = detector.firstMatch(in: self, options: [], range: NSRange(location: 0, length: self.endIndex.encodedOffset)) {
+            // it is a link, if the match covers the whole string
+            return match.range.length == self.endIndex.encodedOffset
+        } else {
+            return false
+        }
+    }
+}
